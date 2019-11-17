9d2aae0b5fc05a46e61758e8524a6fe9ed0e3670
test-music <azlaanzubair4@gmail.com>
2019-11-17 20:52:05 +0500

    Dart Basic Syntax - Basic DataTypes

---
+++ Phase1_DartBasicSyntax/00_DataTypes.dart
@@ -0,0 +1,41 @@
+import 'dart:ffi';
+
+void main() {
+
+  // LIKE EVERY PROGRAMMING LANGUAGE DART ALSO HAVE THESE
+  // KNOWN DATATYPES WHICH ARE AS FOLLOWS
+  //  -  NUMBER
+  //    --  INT
+          int age = 21;
+          var salary = 35000;
+
+  //    --  DOUBLE (WE TREAT IT AS FLOAT)
+          double percentage = 75.5;
+          var cgpa = 3.5;
+
+  //  -  STRING
+        String greet = "Hello World";
+        var name = "Azlaan";
+
+  //  -  BOOLEAN
+        bool isOpen = true;
+        var isClosed = false;
+
+  // WE CAN DEFINE DATATYPE WITH THE INITIALIZING OF A VARIABLE OR
+  // WE CAN JUST INITIALIZE A VARIABLE WITH 'VAR' KEYWORD AND THE
+  // COMPILER AUTOMATICALLY GET THE DATA TYPE BY READING IT'S VALUE
+
+  print("\nStrings:");
+  print("========");
+  print("Name: $name\nGreet: $greet");
+  print("\nNumbers (integer):");
+  print("==================");
+  print("Age: $age\nSalary: $salary");
+  print("\nNumbers (double):");
+  print("=================");
+  print("Percentage: $percentage\nCGPA: $cgpa");
+  print("\nBoolean:");
+  print("========");
+  print("Open: $isOpen\nClose: $isClosed");
+
+}
