f7d00b1a659fe05ded87462a0b14649096dbdb2c
test-music <azlaanzubair4@gmail.com>
2019-11-19 00:26:18 +0500

    all decision making statements are completed.

--- Phase_0_DartBasicSyntax/03_DecisionMaking.dart
+++ Phase_0_DartBasicSyntax/03_DecisionMaking.dart
@@ -1,6 +1,6 @@
 void main() {
 
-  // THERE ARE TWO METHOD OF DECISION MAKING IN DART
+  // THERE ARE THREE METHOD OF DECISION MAKING IN DART
   // LIKE ANY OTHER LANGUAGE
   // METHOD 1 (IF-ELSE LADDER)
 
@@ -14,7 +14,36 @@ void main() {
       print("You are a kid");
     }
 
-  // METHOD 2 (SWITCH STATEMENT)
-  
+  // METHOD 2 (TERNARY OPERATOR)
+    // METHOD 2-A
+    var a = 6;
+    var b = 5;
+
+    // CONDITION ? OUTPUT ONE (if condition is true) : OUPUT TWO (if condition is false)
+    a < b ? print("$a is smaller than $b") : print("$b is smaller than $a");
+
+    // METHOD 2-B
+    // THIS METHOD IS USED WHEN ONE EXPRESSION IS NULL SO THE PROGRAM TAKES DEFAULT VALUE
+    // EXPRESSION 1 ?? EXPRESSION 2
+    var name = "Azlaan";
+    var nameToPrint = name ?? "Guest User";
+    print("Welcome $nameToPrint.");
+
+  // METHOD 3 (SWITCH STATEMENT)
+  var grade = "A";
+
+  switch (grade) {
+    case "A":
+      print("Excelent");
+      break;
+    case "B":
+      print("Good");
+      break;
+    case "C":
+      print("Bad");
+      break;
+    default:
+      print("Poor");
+  }
 
 }
