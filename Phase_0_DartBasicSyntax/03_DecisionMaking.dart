55b696aa04508941adabb66dbe60a98b7218a06a
test-music <azlaanzubair4@gmail.com>
2019-11-18 23:24:46 +0500

    decision making statements in dart | IF-ELSE ladder

---
+++ Phase_0_DartBasicSyntax/03_DecisionMaking.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  // THERE ARE TWO METHOD OF DECISION MAKING IN DART
+  // LIKE ANY OTHER LANGUAGE
+  // METHOD 1 (IF-ELSE LADDER)
+
+    var age = 23;
+
+    if(age >= 20){
+      print("You are an adult");
+    } else if(age < 20 && age >= 13) {
+      print("You are a teenager");
+    } else {
+      print("You are a kid");
+    }
+
+  // METHOD 2 (SWITCH STATEMENT)
+  
+
+}
