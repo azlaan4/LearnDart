b17dd9c7fcc5d36b6a244bfa3254a8cbe657c8c2
test-music <azlaanzubair4@gmail.com>
2019-11-19 21:40:27 +0500

    continue statement of loop

--- Phase_0_DartBasicSyntax/05_BreakAndContinue.dart
+++ Phase_0_DartBasicSyntax/05_BreakAndContinue.dart
@@ -3,12 +3,25 @@ void main () {
   // ARE 'break' AND 'continue'
   // THE 'break' STATEMENT IS USED TO END THE LOOP IMMIDIATELY
   // WHEN IT IS ENCOUNTERED
-  print("Stop loop on first odd number.");
-  List numbers = [2,4,6,8,10,40,14,5,23,20,100,10,12,14];
+  print("\nStop loop on first odd number.");
+  List numbers = [2,4,6,5,23,20,100,10,12,14];
   for (var number in numbers) {
-    print(number);
     if(number % 2 != 0){
       break;
     }
+    print(number);
   }
+
+  // THE 'continue' STATEMENT IS USED TO SKIP THE LOOP ITERATION
+  // WHEN IT IS ENCOUNTERED, IT SKIPS THE CODE WRITE AFTER IT AND
+  // START NEXT ITERATION
+  print("\nPrint only even numbers.");
+  List random_numbers = [1,2,3,4,5,6,7,8,9,10];
+  for (var number in random_numbers) {
+    if(number % 2 != 0){
+      continue;
+    }
+    print(number);
+  }
+
 }
